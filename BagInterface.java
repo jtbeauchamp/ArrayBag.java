d91d02451c32f546059b0851d7fa19ea68908e7f
jtbeauchamp <150178969+jtbeauchamp@users.noreply.github.com>
2025-03-05 17:35:05 -0800

    Update BagInterface.java

--- BagInterface.java
+++ BagInterface.java
@@ -4,7 +4,12 @@
 //  Course: cs-2400-02-F23
 //
 //  Desription:
-//          
+//        The project focuses on the implementation of the bag data structure through the use of an array.
+//        The array bag is a generic class that allows the bag to implement any data type for a safe coding
+//        practice. The array bag implements methods that allow for the manipulation and retreiving of data.
+//        This project's main focus is how to use the array bag to store java keywords, then taking user input
+//        via command line and comparing the input from the user to data in the bag to see if the input is a
+//        java keyword.     
 
 /** An interface that describes the operations of a bag of objects */
 public interface BagInterface <T>{
